51d51c5a53aeed338a07eb8afadf69b8ade37ef3
Udo Woitek <superudo@gmail.com>
2014-10-06 10:36:33 +0200

    Vertical StyledRadioGroup

--- app/src/main/java/de/woitek/freetrader1902scorekeeper/EventActivity.java
+++ app/src/main/java/de/woitek/freetrader1902scorekeeper/EventActivity.java
@@ -21,223 +21,222 @@ import de.woitek.freetrader1902scorekeeper.types.GameFight;
 import de.woitek.libraries.styledradiogroup.StyledRadioGroup;
 
 public class EventActivity extends Activity implements RadioGroup.OnCheckedChangeListener, Observer {
-    public final String CLASSID = "EventActivity";
+	public final String CLASSID = "EventActivity";
 
-    GameData gameData;
-    GameFight gameFight;
-    private ViewSwitcher switcher;
-    private StyledRadioGroup rgAction;
+	GameData gameData;
+	GameFight gameFight;
+	private ViewSwitcher switcher;
+	private StyledRadioGroup rgAction;
 
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int i) {
-        switch (radioGroup.getId()) {
-            case R.id.rgAction:
-                switchAction(i);
-                break;
-            case R.id.rgTask:
-                switchTask(i);
-                break;
-            case R.id.rgMultiplier:
-                switchMultiplier(i);
-                break;
-            case R.id.rgYourStat:
-                switchYourStat(i);
-                break;
-            case R.id.rgTheirStat:
-                switchTheirStat(i);
-                break;
-            case R.id.rgConstableFine:
-                switchConstableFine(i);
-                break;
-            case R.id.rgLawLevel:
-                switchLawLevel(i);
-            default:
-                Log.d(CLASSID, String.format("Unknown RadioGroupId: %d", radioGroup.getId()));
-                break;
-        }
-    }
+	@Override
+	public void onCheckedChanged(RadioGroup radioGroup, int i) {
+		switch (radioGroup.getId()) {
+			case R.id.rgAction:
+				switchAction(i);
+				break;
+			case R.id.rgTask:
+				switchTask(i);
+				break;
+			case R.id.rgMultiplier:
+				switchMultiplier(i);
+				break;
+			case R.id.rgYourStat:
+				switchYourStat(i);
+				break;
+			case R.id.rgTheirStat:
+				switchTheirStat(i);
+				break;
+			case R.id.rgConstableFine:
+				switchConstableFine(i);
+				break;
+			case R.id.rgLawLevel:
+				switchLawLevel(i);
+			default:
+				Log.d(CLASSID, String.format("Unknown RadioGroupId: %d", radioGroup.getId()));
+				break;
+		}
+	}
 
-    private void switchLawLevel(int lawLevel) {
+	private void switchLawLevel(int lawLevel) {
 
-    }
+	}
 
-    private void switchConstableFine(int fine) {
+	private void switchConstableFine(int fine) {
+	}
 
-    }
+	private void switchTheirStat(int selectedButtonIndex) {
+		gameFight.setEnemyModifier(selectedButtonIndex + 1);
+	}
 
-    private void switchTheirStat(int selectedButtonIndex) {
-        gameFight.setEnemyModifier(selectedButtonIndex + 1);
-    }
+	private void switchYourStat(int selectedButtonIndex) {
+		gameFight.setPlayerModifier(selectedButtonIndex + 1);
+	}
 
-    private void switchYourStat(int selectedButtonIndex) {
-        gameFight.setPlayerModifier(selectedButtonIndex + 1);
-    }
+	private void switchMultiplier(int selectedButtonIndex) {
+		gameFight.setEnemyMight(selectedButtonIndex + 1);
+	}
 
-    private void switchMultiplier(int selectedButtonIndex) {
-        gameFight.setEnemyMight(selectedButtonIndex + 1);
-    }
+	private void switchTask(int selectedButtonIndex) {
+		((RadioGroup) findViewById(R.id.rgYourStat)).clearCheck();
+		((RadioGroup) findViewById(R.id.rgTheirStat)).clearCheck();
+		GameFight.Fight fight;
+		switch (selectedButtonIndex) {
+			case 0:
+				fight = GameFight.Fight.DEFENSE;
+				break;
+			case 1:
+				fight = GameFight.Fight.OFFENSE;
+				break;
+			default:
+				fight = GameFight.Fight.NOFIGHT;
+				break;
+		}
+		updateTextViews(fight);
+		gameFight.setFight(fight);
+	}
 
-    private void switchTask(int selectedButtonIndex) {
-        ((RadioGroup) findViewById(R.id.rgYourStat)).clearCheck();
-        ((RadioGroup) findViewById(R.id.rgTheirStat)).clearCheck();
-        GameFight.Fight fight;
-        switch (selectedButtonIndex) {
-            case 0:
-                fight = GameFight.Fight.DEFENSE;
-                break;
-            case 1:
-                fight = GameFight.Fight.OFFENSE;
-                break;
-            default:
-                fight = GameFight.Fight.NOFIGHT;
-                break;
-        }
-        updateTextViews(fight);
-        gameFight.setFight(fight);
-    }
+	private void updateTextViews(GameFight.Fight fight) {
+		SparseArray<String> a = new SparseArray<String>();
+		switch (fight) {
+			case OFFENSE:
+				a.append(R.id.lblYourBonus, String.format("%s: %d", GameData.SHOTGUNS, gameData.getEquipment(GameData.SHOTGUNS)));
+				a.append(R.id.lblTheirBonus, String.format("MONTH BONUS: %d", gameData.getMonth() - 1));
+				a.append(R.id.lblYourStateType, "AGGRESSION");
+				a.append(R.id.lblTheirStateType, "DEFENSE");
+				a.append(R.id.lblYourFinalStat, "ATTACK");
+				a.append(R.id.lblTheirFinalStat, "DEFENSE");
+				break;
+			case DEFENSE:
+				a.append(R.id.lblYourBonus, String.format("%s: %d", GameData.ARMOR, gameData.getEquipment(GameData.ARMOR)));
+				a.append(R.id.lblTheirBonus, String.format("MONTH BONUS: %d", gameData.getMonth() - 1));
+				a.append(R.id.lblYourStateType, "DEFENSE");
+				a.append(R.id.lblTheirStateType, "AGGRESSION");
+				a.append(R.id.lblYourFinalStat, "DEFENSE");
+				a.append(R.id.lblTheirFinalStat, "ATTACK");
+				break;
+			case NOFIGHT:
+				a.append(R.id.lblYourBonus, "ARMOR / SHOTGUNS");
+				a.append(R.id.lblTheirBonus, "MONTH BONUS");
+				a.append(R.id.lblYourStateType, "MODIFIER");
+				a.append(R.id.lblTheirStateType, "MODIFIER");
+				a.append(R.id.lblYourFinalStat, "YOURS");
+				a.append(R.id.lblTheirFinalStat, "THEIRS");
+				a.append(R.id.tYourFinalStat, "0");
+				a.append(R.id.tTheirFinalStat, "0");
+				a.append(R.id.lblRules, "");
+				a.append(R.id.lblOutcome, "SELECT TASK AND MODIFIERS");
+				break;
+		}
+		final int N = a.size();
+		for (int i = 0; i < N; i++) {
+			((TextView) findViewById(a.keyAt(i))).setText(a.valueAt(i));
+		}
+	}
 
-    private void updateTextViews(GameFight.Fight fight) {
-        SparseArray<String> a = new SparseArray<String>();
-        switch (fight) {
-            case OFFENSE:
-                a.append(R.id.lblYourBonus, String.format("%s: %d", GameData.SHOTGUNS, gameData.getEquipment(GameData.SHOTGUNS)));
-                a.append(R.id.lblTheirBonus, String.format("MONTH BONUS: %d", gameData.getMonth() - 1));
-                a.append(R.id.lblYourStateType, "AGGRESSION");
-                a.append(R.id.lblTheirStateType, "DEFENSE");
-                a.append(R.id.lblYourFinalStat, "ATTACK");
-                a.append(R.id.lblTheirFinalStat, "DEFENSE");
-                break;
-            case DEFENSE:
-                a.append(R.id.lblYourBonus, String.format("%s: %d", GameData.ARMOR, gameData.getEquipment(GameData.ARMOR)));
-                a.append(R.id.lblTheirBonus, String.format("MONTH BONUS: %d", gameData.getMonth() - 1));
-                a.append(R.id.lblYourStateType, "DEFENSE");
-                a.append(R.id.lblTheirStateType, "AGGRESSION");
-                a.append(R.id.lblYourFinalStat, "DEFENSE");
-                a.append(R.id.lblTheirFinalStat, "ATTACK");
-                break;
-            case NOFIGHT:
-                a.append(R.id.lblYourBonus, "ARMOR / SHOTGUNS");
-                a.append(R.id.lblTheirBonus, "MONTH BONUS");
-                a.append(R.id.lblYourStateType, "MODIFIER");
-                a.append(R.id.lblTheirStateType, "MODIFIER");
-                a.append(R.id.lblYourFinalStat, "YOURS");
-                a.append(R.id.lblTheirFinalStat, "THEIRS");
-                a.append(R.id.tYourFinalStat, "0");
-                a.append(R.id.tTheirFinalStat, "0");
-                a.append(R.id.lblRules, "");
-                a.append(R.id.lblOutcome, "SELECT TASK AND MODIFIERS");
-                break;
-        }
-        final int N = a.size();
-        for (int i = 0; i < N; i++) {
-            ((TextView) findViewById(a.keyAt(i))).setText(a.valueAt(i));
-        }
-    }
+	private void switchAction(int selectedButtonIndex) {
+		switch (selectedButtonIndex) {
+			case 0: // Bandit
+				gameFight.setEnemy(GameFight.Enemy.HIGHWAYMAN);
+				findViewById(R.id.rgMultiplier).setVisibility(View.VISIBLE);
+				switcher.setDisplayedChild(1);
+				break;
+			case 1: // Bear
+				gameFight.setEnemy(GameFight.Enemy.BEAR);
+				findViewById(R.id.rgMultiplier).setVisibility(View.INVISIBLE);
+				switcher.setDisplayedChild(1);
+				break;
+			case 2: // Police
+				switcher.setDisplayedChild(0);
+				break;
+		}
+	}
 
-    private void switchAction(int selectedButtonIndex) {
-        switch (selectedButtonIndex) {
-            case 0: // Bandit
-                gameFight.setEnemy(GameFight.Enemy.HIGHWAYMAN);
-                findViewById(R.id.rgMultiplier).setVisibility(View.VISIBLE);
-                switcher.setDisplayedChild(1);
-                break;
-            case 1: // Bear
-                gameFight.setEnemy(GameFight.Enemy.BEAR);
-                findViewById(R.id.rgMultiplier).setVisibility(View.INVISIBLE);
-                switcher.setDisplayedChild(1);
-                break;
-            case 2: // Police
-                switcher.setDisplayedChild(0);
-                break;
-        }
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event);
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event);
+		gameData = getIntent().getParcelableExtra(GameData.CLASSNAME);
+		if (gameData == null) {
+			gameData = new GameData();
+		}
+		gameFight = new GameFight(gameData);
 
-        gameData = getIntent().getParcelableExtra(GameData.CLASSNAME);
-        if (gameData == null) {
-            gameData = new GameData();
-        }
-        gameFight = new GameFight(gameData);
+		ActionBar b = getActionBar();
+		if (b != null) {
+			b.setDisplayUseLogoEnabled(false);
+			b.setDisplayHomeAsUpEnabled(true);
+		}
 
-        ActionBar b = getActionBar();
-        if (b != null) {
-            b.setDisplayUseLogoEnabled(false);
-            b.setDisplayHomeAsUpEnabled(true);
-        }
+		switcher = (ViewSwitcher) findViewById(R.id.viewSwitcher);
+		switcher.reset();
 
-        switcher = (ViewSwitcher) findViewById(R.id.viewSwitcher);
-        switcher.reset();
+		rgAction = (StyledRadioGroup) findViewById(R.id.rgAction);
+		rgAction.setOnCheckedChangeListener(this);
 
-        rgAction = (StyledRadioGroup) findViewById(R.id.rgAction);
-        rgAction.setOnCheckedChangeListener(this);
+		((RadioGroup) findViewById(R.id.rgMultiplier)).setOnCheckedChangeListener(this);
+		((RadioGroup) findViewById(R.id.rgTask)).setOnCheckedChangeListener(this);
+		((RadioGroup) findViewById(R.id.rgYourStat)).setOnCheckedChangeListener(this);
+		((RadioGroup) findViewById(R.id.rgTheirStat)).setOnCheckedChangeListener(this);
+		((RadioGroup) findViewById(R.id.rgLawLevel)).setOnCheckedChangeListener(this);
+		((RadioGroup) findViewById(R.id.rgConstableFine)).setOnCheckedChangeListener(this);
 
-        ((RadioGroup) findViewById(R.id.rgMultiplier)).setOnCheckedChangeListener(this);
-        ((RadioGroup) findViewById(R.id.rgTask)).setOnCheckedChangeListener(this);
-        ((RadioGroup) findViewById(R.id.rgYourStat)).setOnCheckedChangeListener(this);
-        ((RadioGroup) findViewById(R.id.rgTheirStat)).setOnCheckedChangeListener(this);
-        ((RadioGroup) findViewById(R.id.rgLawLevel)).setOnCheckedChangeListener(this);
-        ((RadioGroup) findViewById(R.id.rgConstableFine)).setOnCheckedChangeListener(this);
+		gameFight.addObserver(this);
+	}
 
-        gameFight.addObserver(this);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				Intent mainIntent = new Intent(this, MainActivity.class);
+				mainIntent.putExtra("GameData", gameData);
+				startActivity(mainIntent);
+				finish();
+			default:
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Intent mainIntent = new Intent(this, MainActivity.class);
-                mainIntent.putExtra("GameData", gameData);
-                startActivity(mainIntent);
-                finish();
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.event, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.event, menu);
-        return true;
-    }
+	@Override
+	public void update(Observable observable, Object o) {
+		SparseArray<String> a = new SparseArray<String>();
+		if (gameFight.canFight()) { // Fight result
+			a.append(R.id.tYourFinalStat, String.format("%d", gameFight.getPlayerFightValue()));
+			a.append(R.id.tTheirFinalStat, String.format("%d", gameFight.getEnemyFightValue()));
+			if (gameFight.hasPlayerWon()) {
+				a.append(R.id.lblOutcome, "YOU HAVE WON THE FIGHT");
+				if (gameFight.getFight() == GameFight.Fight.DEFENSE) {
+					a.append(R.id.lblRules, "You may now fight back or try to flee by drawing another event card.");
+				} else { // OFFENSE
+					if (gameFight.getEnemy() == GameFight.Enemy.HIGHWAYMAN) {
+						a.append(R.id.lblRules,
+								String.format("You get $ %d as a bonus for killing these bandits. You may now proceed to the next city. Use the sell modifiers at the bandit card to calculate prices.",
+										gameFight.getEnemyBonusValue()));
+					} else {
+						a.append(R.id.lblRules, "You may now proceed to the next city. Use the sell modifiers at the bear card to calculate prices.");
+					}
+				}
+			} else {
+				a.append(R.id.lblOutcome, "YOU LOST THE FIGHT");
+				a.append(R.id.lblRules, "You loose one part of your truck. You may now fight back or try to flee by drawing another event card.");
+			}
+		} else { // No fight
+			a.append(R.id.tYourFinalStat, "0");
+			a.append(R.id.tTheirFinalStat, "0");
+			a.append(R.id.lblRules, "");
+			a.append(R.id.lblOutcome, "");
+		}
 
-    @Override
-    public void update(Observable observable, Object o) {
-        SparseArray<String> a = new SparseArray<String>();
-        if (gameFight.canFight()) { // Fight result
-            a.append(R.id.tYourFinalStat, String.format("%d", gameFight.getPlayerFightValue()));
-            a.append(R.id.tTheirFinalStat, String.format("%d", gameFight.getEnemyFightValue()));
-            if (gameFight.hasPlayerWon()) {
-                a.append(R.id.lblOutcome, "YOU HAVE WON THE FIGHT");
-                if (gameFight.getFight() == GameFight.Fight.DEFENSE) {
-                    a.append(R.id.lblRules, "You may now fight back or try to flee by drawing another event card.");
-                } else { // OFFENSE
-                    if (gameFight.getEnemy() == GameFight.Enemy.HIGHWAYMAN) {
-                        a.append(R.id.lblRules,
-                                String.format("You get $ %d as a bonus for killing these bandits. You may now proceed to the next city. Use the sell modifiers at the bandit card to calculate prices.",
-                                        gameFight.getEnemyBonusValue()));
-                    } else {
-                        a.append(R.id.lblRules, "You may now proceed to the next city. Use the sell modifiers at the bear card to calculate prices.");
-                    }
-                }
-            } else {
-                a.append(R.id.lblOutcome, "YOU LOST THE FIGHT");
-                a.append(R.id.lblRules, "You loose one part of your truck. You may now fight back or try to flee by drawing another event card.");
-            }
-        } else { // No fight
-            a.append(R.id.tYourFinalStat, "0");
-            a.append(R.id.tTheirFinalStat, "0");
-            a.append(R.id.lblRules, "");
-            a.append(R.id.lblOutcome, "");
-        }
-
-        final int N = a.size();
-        for (int i = 0; i < N; i++) {
-            ((TextView) findViewById(a.keyAt(i))).setText(a.valueAt(i));
-        }
-    }
+		final int N = a.size();
+		for (int i = 0; i < N; i++) {
+			((TextView) findViewById(a.keyAt(i))).setText(a.valueAt(i));
+		}
+	}
 }
--- styledradiogroup/src/main/java/de/woitek/libraries/styledradiogroup/StyledRadioGroup.java
+++ styledradiogroup/src/main/java/de/woitek/libraries/styledradiogroup/StyledRadioGroup.java
@@ -6,137 +6,170 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.StateListDrawable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class StyledRadioGroup extends RadioGroup {
-    private String mDisplay[] = new String[]{"1", "2"};
-    private String mValues[] = null;
+	private String mDisplay[] = new String[]{"1", "2"};
+	private String mValues[] = null;
+	private eDirection mDirection;
 
-    public StyledRadioGroup(Context context) {
-        super(context);
-        init(null, 0);
-    }
+	public StyledRadioGroup(Context context) {
+		super(context);
+		init(null, 0);
+	}
 
-    public StyledRadioGroup(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(attrs, 0);
-    }
+	public StyledRadioGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs, 0);
+	}
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.StyledRadioGroup, defStyle, 0);
+	private void init(AttributeSet attrs, int defStyle) {
+		// Load attributes
+		final TypedArray a = getContext().obtainStyledAttributes(
+				attrs, R.styleable.StyledRadioGroup, defStyle, 0);
 
-        if (a.hasValue(R.styleable.StyledRadioGroup_display)) {
-            mDisplay = a.getString(R.styleable.StyledRadioGroup_display).split(";");
-            if (mDisplay.length < 2) {
-                mDisplay = new String[]{"1", "2"};
-            }
-        }
+		if (a.hasValue(R.styleable.StyledRadioGroup_display)) {
+			mDisplay = a.getString(R.styleable.StyledRadioGroup_display).split(";");
+			if (mDisplay.length < 2) {
+				mDisplay = new String[]{"1", "2"};
+			}
+		}
 
-        if (a.hasValue(R.styleable.StyledRadioGroup_values)) {
-            mValues = a.getString(R.styleable.StyledRadioGroup_values).split(";");
-        }
-        if ((mValues == null) || (mValues.length == 0)) {
-            mValues = mDisplay;
-        }
+		if (a.hasValue(R.styleable.StyledRadioGroup_values)) {
+			mValues = a.getString(R.styleable.StyledRadioGroup_values).split(";");
+		}
+		if ((mValues == null) || (mValues.length == 0)) {
+			mValues = mDisplay;
+		}
 
-        boolean initialSelect = true;
-        if (a.hasValue(R.styleable.StyledRadioGroup_selected)) {
-            initialSelect = a.getBoolean(R.styleable.StyledRadioGroup_selected, true);
-        }
+		boolean initialSelect = true;
+		if (a.hasValue(R.styleable.StyledRadioGroup_selected)) {
+			initialSelect = a.getBoolean(R.styleable.StyledRadioGroup_selected, true);
+		}
 
-        a.recycle();
+		mDirection = eDirection.VERTICAL;
+		if (a.hasValue(a.getInteger(R.styleable.StyledRadioGroup_direction, 0))) {
+			int nDirection = a.getInteger(R.styleable.StyledRadioGroup_direction, 0);
+			mDirection = (nDirection == 0)
+					? eDirection.HORIZONTAL
+					: eDirection.VERTICAL;
+		}
 
-        setOrientation(HORIZONTAL);
+		a.recycle();
 
-        final int displayLength = mDisplay.length;
-        final int valuesLength = mValues.length;
-        final Context ctx = getContext();
+		final int displayLength = mDisplay.length;
+		final int valuesLength = mValues.length;
+		final Context ctx = getContext();
 
-        for (int i = 0; i < displayLength; ++i) {
-            RadioButton rb = new RadioButton(ctx);
-            rb.setId(i);
-            rb.setText(mDisplay[i]);
-            rb.setGravity(TEXT_ALIGNMENT_CENTER);
-            rb.setLayoutParams(new LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT, 1f));
-            int bgDrwId = R.drawable.button_background;
-            if (i == 0) {
-                bgDrwId = R.drawable.button_background_left;
-            } else if (i == displayLength - 1) {
-                bgDrwId = R.drawable.button_background_right;
-            }
-            rb.setBackground(getResources().getDrawable(bgDrwId));
-            rb.setTextColor(new ColorStateList(
-                    new int[][]{
-                            new int[]{android.R.attr.state_checked},
-                            new int[]{}
-                    },
-                    new int[]{
-                            Color.WHITE,
-                            Color.BLACK
-                    }
-            ));
+		int orientation = HORIZONTAL;
+		int bgFirstDrawable = R.drawable.button_background_left;
+		int bgMiddleDrawable = R.drawable.button_background_horizontal;
+		int bgLastDrawable = R.drawable.button_background_right;
+		int width = WRAP_CONTENT;
+		int height = MATCH_PARENT;
 
-            rb.setButtonDrawable(new StateListDrawable());
-            rb.setTag((i < valuesLength) ? mValues[i] : mDisplay[i]);
-            addView(rb);
-        }
+		if (mDirection == eDirection.VERTICAL) {
+			orientation = VERTICAL;
+			bgFirstDrawable = R.drawable.button_background_top;
+			bgMiddleDrawable = R.drawable.button_background_vertical;
+			bgLastDrawable = R.drawable.button_background_bottom;
+			width = MATCH_PARENT;
+			height = WRAP_CONTENT;
+		}
 
-        setSelectedIndex((initialSelect) ? 0 : -1);
-    }
+		setOrientation(orientation);
 
-    public String getSelectedItemValue() {
-        RadioButton rb = (RadioButton) findViewById(getSelectedIndex());
-        return (rb != null) ? (String) rb.getTag() : null;
-    }
+		for (int i = 0; i < displayLength; ++i) {
+			RadioButton rb = new RadioButton(ctx);
+			rb.setId(i);
+			rb.setText(mDisplay[i]);
+			rb.setGravity(Gravity.CENTER);
 
-    public void setSelectedItemValue(String value) {
-        RadioButton rb = (RadioButton) findViewById(getSelectedIndex());
-        if (rb != null) {
-            rb.setTag(value);
-        }
-    }
+			rb.setLayoutParams(new LinearLayout.LayoutParams(width, height, 1f));
+			int bgDrwId = bgMiddleDrawable;
+			if (i == 0) {
+				bgDrwId = bgFirstDrawable;
+			} else if (i == displayLength - 1) {
+				bgDrwId = bgLastDrawable;
+			}
+			rb.setBackground(getResources().getDrawable(bgDrwId));
+			rb.setTextColor(new ColorStateList(
+					new int[][]{
+							new int[]{android.R.attr.state_checked},
+							new int[]{}
+					},
+					new int[]{
+							Color.WHITE,
+							Color.BLACK
+					}
+			));
 
-    public String getSelectedItemText() {
-        RadioButton rb = (RadioButton) findViewById(getSelectedIndex());
-        return (String) rb.getTag();
-    }
+			rb.setButtonDrawable(new StateListDrawable());
+			rb.setTag((i < valuesLength) ? mValues[i] : mDisplay[i]);
+			addView(rb);
+		}
 
-    public int getSelectedIndex() {
-        return getCheckedRadioButtonId();
-    }
+		setSelectedIndex((initialSelect) ? 0 : -1);
+	}
 
-    public void setSelectedIndex(int index) {
-        if (index < 0) {
-            clearCheck();
-        } else if (index < mDisplay.length) {
-            check(index);
-        }
-    }
+	public String getSelectedItemValue() {
+		RadioButton rb = (RadioButton) findViewById(getSelectedIndex());
+		return (rb != null) ? (String) rb.getTag() : null;
+	}
 
-    public int getMaxIndex() {
-        return mDisplay.length;
-    }
+	public void setSelectedItemValue(String value) {
+		RadioButton rb = (RadioButton) findViewById(getSelectedIndex());
+		if (rb != null) {
+			rb.setTag(value);
+		}
+	}
 
-    public void enableAll(boolean enable) {
-        final int N = getChildCount();
-        for (int i = 0; i < N; ++i) {
-            ((RadioButton) getChildAt(i)).setEnabled(enable);
-        }
-        setSelectedIndex((enable) ? 0 : -1);
-    }
+	public String getSelectedItemText() {
+		RadioButton rb = (RadioButton) findViewById(getSelectedIndex());
+		return (String) rb.getTag();
+	}
 
-    public void enableIndex(int index, boolean enabled) {
-        if (!enabled && (getSelectedIndex() == index)) {
-            clearCheck();
-        }
-        if ((index >= 0) && (index < mDisplay.length)) {
-            ((RadioButton) getChildAt(index)).setEnabled(enabled);
-        }
-    }
+	public int getSelectedIndex() {
+		return getCheckedRadioButtonId();
+	}
+
+	public void setSelectedIndex(int index) {
+		if (index < 0) {
+			clearCheck();
+		} else if (index < mDisplay.length) {
+			check(index);
+		}
+	}
+
+	public int getMaxIndex() {
+		return mDisplay.length;
+	}
+
+	public void enableAll(boolean enable) {
+		final int N = getChildCount();
+		for (int i = 0; i < N; ++i) {
+			getChildAt(i).setEnabled(enable);
+		}
+		setSelectedIndex((enable) ? 0 : -1);
+	}
+
+	public void enableIndex(int index, boolean enabled) {
+		if (!enabled && (getSelectedIndex() == index)) {
+			clearCheck();
+		}
+		if ((index >= 0) && (index < mDisplay.length)) {
+			getChildAt(index).setEnabled(enabled);
+		}
+	}
+
+	private enum eDirection {
+		HORIZONTAL,
+		VERTICAL
+	}
 }
